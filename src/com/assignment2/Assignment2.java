13f9f26cf3ad2e541d98df5ffdf8c8580469fb55
Sam Azizi <sadeghazizi.ca@gmail.com>
2022-11-15 19:27:12 -0800

    this is a first version of assignment 2

--- src/com/assignment2/Assignment2.java
+++ src/com/assignment2/Assignment2.java
@@ -1,11 +1,59 @@
 package com.assignment2;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Assignment2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-       // Lets Start New Project
-		// this is new Good Project
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Pick a number between 1 and 100");
+		int userGuessNumber = inputMethod(scanner);
+		
+		//Generate Random Number
+		Random randomNum = new Random();
+		int randomNumber = randomNum.nextInt(100);
+		
+		//Checking that the selected number is in the correct range
+		while (userGuessNumber < 1 | userGuessNumber > 100) {
+			System.out.println("Your guess is not between 1 and 100, please try again");
+			userGuessNumber = inputMethod(scanner);
+		}
+		//Start Game
+		Boolean check = true;
+		int counter = 1;
+		while (check & (counter < 5)) {
+			check = compareMethod(userGuessNumber, randomNumber);
+			userGuessNumber = inputMethod(scanner);
+			counter++;
+		}
+		//End Game
+		System.out.println("You lose!");
+		System.out.println("The number to guess was:" + randomNumber);
+		scanner.close();
+
+	}
+	//Get Guessed Number 
+	static int inputMethod(Scanner userGuess) {
+		String userGuessString = userGuess.nextLine();
+		int userGuessNumber = Integer.parseInt(userGuessString);
+		return userGuessNumber;
+	}
+	
+	//Compare guessed and Random Number
+	static boolean compareMethod(int userGuessNumber, int randomNumber) {
+		if (userGuessNumber == randomNumber) {
+			System.out.println("You win!");
+			System.exit(0);
+			return false;
+
+		} else if (userGuessNumber < randomNumber) {
+			System.out.println("Please pick a higher number");
+
+		} else if (userGuessNumber > randomNumber) {
+			System.out.println("Please pick a lower number");
+		}
+		return true;
 	}
 
 }
