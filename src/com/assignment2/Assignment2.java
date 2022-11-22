110c332bdc18706e68a2954515878e453f7c09b4
Sam Azizi <sadeghazizi.ca@gmail.com>
2022-11-21 20:17:37 -0800

    update 1 after bug fixes

--- src/com/assignment2/Assignment2.java
+++ src/com/assignment2/Assignment2.java
@@ -9,44 +9,50 @@ public class Assignment2 {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Pick a number between 1 and 100");
 		int userGuessNumber = inputMethod(scanner);
-		
-		//Generate Random Number
+
+		// Generate Random Number
 		Random randomNum = new Random();
 		int randomNumber = randomNum.nextInt(100);
-		
-		//Checking that the selected number is in the correct range
-		while (userGuessNumber < 1 | userGuessNumber > 100) {
-			System.out.println("Your guess is not between 1 and 100, please try again");
-			userGuessNumber = inputMethod(scanner);
+		System.out.println(randomNumber);
+
+		for (int counter = 0; counter < 5; counter++) {
+			if (userGuessNumber == randomNumber) {
+				System.out.println("You win!");
+				break;
+			} else if (userGuessNumber < randomNumber) {
+				System.out.println("Please pick a higher number");
+			} else if (userGuessNumber > randomNumber) {
+				System.out.println("Please pick a lower number");
+			}
+			if (counter < 4) {
+				userGuessNumber = inputMethod(scanner);
+			}
 		}
-		//Start Game
-		Boolean check = true;
-		int counter = 1;
-		while (check & (counter < 5)) {
-			check = compareMethod(userGuessNumber, randomNumber);
-			userGuessNumber = inputMethod(scanner);
-			counter++;
+		if (userGuessNumber != randomNumber) {
+			System.out.println("You lose!");
 		}
-		//End Game
-		System.out.println("You lose!");
 		System.out.println("The number to guess was:" + randomNumber);
 		scanner.close();
 
 	}
-	//Get Guessed Number 
+
+	// Get Guessed Number
 	static int inputMethod(Scanner userGuess) {
 		String userGuessString = userGuess.nextLine();
 		int userGuessNumber = Integer.parseInt(userGuessString);
+		while (userGuessNumber < 1 | userGuessNumber > 100) {
+			System.out.println("Your guess is not between 1 and 100, please try again");
+			userGuessString = userGuess.nextLine();
+			userGuessNumber = Integer.parseInt(userGuessString);
+		}
 		return userGuessNumber;
 	}
-	
-	//Compare guessed and Random Number
+
+	// Compare guessed and Random Number
 	static boolean compareMethod(int userGuessNumber, int randomNumber) {
 		if (userGuessNumber == randomNumber) {
 			System.out.println("You win!");
-			System.exit(0);
 			return false;
-
 		} else if (userGuessNumber < randomNumber) {
 			System.out.println("Please pick a higher number");
 
