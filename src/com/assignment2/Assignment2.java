8a83f9e9d7e3c156ca67202b9e04838420063964
Sam Azizi <sadeghazizi.ca@gmail.com>
2023-01-03 17:05:07 -0800

    last

--- src/com/assignment2/Assignment2.java
+++ src/com/assignment2/Assignment2.java
@@ -7,14 +7,15 @@ public class Assignment2 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Pick a number between 1 and 100");
-		int userGuessNumber = inputMethod(scanner);
+		int userGuessNumber = 0;
 
-		// Generate Random Number
+// Generate Random Number
 		Random randomNum = new Random();
-		int randomNumber = randomNum.nextInt(100);
+		int randomNumber = randomNum.nextInt(100) + 1;
+		System.out.println(randomNumber);
 
 		for (int counter = 0; counter < 5; counter++) {
+			userGuessNumber = inputMethod(scanner);
 			if (userGuessNumber == randomNumber) {
 				System.out.println("You win!");
 				break;
@@ -23,23 +24,26 @@ public class Assignment2 {
 			} else if (userGuessNumber > randomNumber) {
 				System.out.println("Please pick a lower number");
 			}
-			if (counter < 4) {
-				userGuessNumber = inputMethod(scanner);
-			}
 		}
 		if (userGuessNumber != randomNumber) {
 			System.out.println("You lose!");
 			System.out.println("The number to guess was:" + randomNumber);
 		}
-
 		scanner.close();
-
 	}
 
-	// Get Guessed Number
-	static int inputMethod(Scanner userGuess) {
+// Get Guessed Number method
+	private static int inputMethod(Scanner userGuess) {
+		System.out.println("Pick a number between 1 and 100");
 		String userGuessString = userGuess.nextLine();
 		int userGuessNumber = Integer.parseInt(userGuessString);
+		userGuessNumber = validationMethod(userGuess, userGuessNumber);
+		return userGuessNumber;
+	}
+
+	// validation method
+	private static int validationMethod(Scanner userGuess, int userGuessNumber) {
+		String userGuessString;
 		while (userGuessNumber < 1 | userGuessNumber > 100) {
 			System.out.println("Your guess is not between 1 and 100, please try again");
 			userGuessString = userGuess.nextLine();
